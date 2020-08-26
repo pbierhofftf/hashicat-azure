e6348aa1ab99422c9514f3498d08d53d3f58e57a
pbierhofftf <70284890+pbierhofftf@users.noreply.github.com>
2020-08-26 14:39:33 -0400

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/philip-training/network/azurerm"
-  version = "3.1.1"
+  version = "3.0.1"
   resource_group_name = "mynetwork"
   address_space       = "10.0.0.0/16"
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
